c714f3db164da33b729f5644fa330d3c2d055f0d
dave111223 <dave111223@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-21 11:26:50 +0000

    Mask warnings for CURLOPT_FOLLOWLOCATION in safe mode.
    git-svn-id: https://plugins.svn.wordpress.org/bitcoin-thai-payment-gateway/trunk@1140509 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/bitcointhai.php
+++ includes/bitcointhai.php
@@ -99,7 +99,7 @@ class bitcointhaiAPI
 	private function apiFetch($action,$params){
 		if($ch = curl_init ()){
 			curl_setopt ($ch, CURLOPT_URL, $this->api_url.'?action='.$action);
-			curl_setopt ($ch, CURLOPT_FOLLOWLOCATION, true );
+			@curl_setopt ($ch, CURLOPT_FOLLOWLOCATION, true );
 			curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1 );
 			curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, 5 );
 			curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, false);
